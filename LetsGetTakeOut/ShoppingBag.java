4f0183ca9300c245f017f8d32f4c42a689850feb
llucasBorba <lucasborba6075@gmail.com>
2023-12-18 21:14:18 -0300

    Total PRice Method

--- LetsGetTakeOut/ShoppingBag.java
+++ LetsGetTakeOut/ShoppingBag.java
@@ -11,4 +11,22 @@ public class ShoppingBag <T extends PricedItem<Integer>>  {
         shoppingBag = new HashMap<>();
     }
 
+    public void addItem(T item){
+        if(shoppingBag.containsKey(item)){
+            int currentQuantity = shoppingBag.get(item);
+            shoppingBag.put(item, currentQuantity + 1);
+         } else {
+            shoppingBag.put(item,1);
+        }
+    }
+    public Integer getTotalPrice(){
+        Integer bagPrice = 0;
+
+        for (Map.Entry<T, Integer> entry : shoppingBag.entrySet()){
+            int quantity =+ entry.getValue();
+            bagPrice =+  entry.getKey().getPrice() * quantity;
+        }
+        return bagPrice;
+    }
+
 }

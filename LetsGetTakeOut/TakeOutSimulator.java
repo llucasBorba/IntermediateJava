abe8ab4a7476e047b97bbfef6c9b2b07ac53ac67
llucasBorba <lucasborba6075@gmail.com>
2023-12-20 13:30:42 -0300

    New method

--- LetsGetTakeOut/TakeOutSimulator.java
+++ LetsGetTakeOut/TakeOutSimulator.java
@@ -1,8 +1,9 @@
 package LetsGetTakeOut;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class TakeOutSimulator <T> {
+public class TakeOutSimulator {
 
     private Customer customer;
     private FoodMenu menu;
@@ -15,7 +16,18 @@ public class TakeOutSimulator <T> {
     }
 
     private <T> T getOutputOnIntInput(String userInputPrompt, IntUserInputRetriver<T> intUserInputRetriver) {
+         int userInput = 0;
+        do{
+            try {
+                System.out.println(userInputPrompt);
+                System.out.println("Your order: ");
+                userInput = input.nextInt();
 
-        return null;
+               return intUserInputRetriver.produceOutputOnIntUserInput(userInput);
+            }catch (IllegalArgumentException e){
+                System.out.printf("%d is not a valid input. Try again", userInput);
+            }
+        }while(true);
+
     }
 }

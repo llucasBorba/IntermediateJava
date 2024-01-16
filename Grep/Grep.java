7d3d18f3825f7fb6b923d3bd1d0b52628dfe83a1
llucasBorba <lucasborba6075@gmail.com>
2024-01-16 14:29:58 -0300

    All done in GREP

--- Grep/Grep.java
+++ Grep/Grep.java
@@ -17,14 +17,19 @@ public class Grep {
         Scanner input;
         String content = "";
 
-        for(int i = 1; i < 5; i++){
-            fileName = "TestFile" + i + ".txt";
+        for(int i = 1; i < 6; i++){
+            fileName = "/Users/lucasborba/Desktop/IntermediateJava/Grep/TestFile" + i + ".txt";
             try {
                 file = new File(fileName);
                 input = new Scanner(file);
                 while(input.hasNext()){
                      content = input.nextLine();
-                    System.out.println(content);
+                   // System.out.println(fileName.substring(48) + " - " + content);
+                    Pattern pattern = Pattern.compile("\\d+");
+                    Matcher matcher = pattern.matcher(content);
+                    if (matcher.find()){
+                        System.out.println(fileName.substring(48) + " - " + content);
+                    }
                 }
             }catch (FileNotFoundException e ){
                 System.out.println("File not found " + fileName);
